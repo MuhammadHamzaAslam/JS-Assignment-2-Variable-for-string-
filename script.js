8d7b262e92d87d3069942705f4b4f9a577532732
MuhammadHamzaAslam <hamzaaslam19087@gmail.com>
2024-02-28 22:23:11 +0500

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var userName = "Hamza"
+var myName = "Muhammad Hamza"
+alert(myName)
+var message = "Hello World"
+alert(message)
+var age = "14 years old"
+alert(age)
+var course = "Web and Mobile App Development"
+alert(course)
+var pizza ="Pizza \n Pizz \n Piz \n Pi \n P"
+alert(pizza)
+var x = "My Email Address is"
+var y = " hamzaaslam19087@gmail.com"
+var z = x+y
+alert(z)
+var a = "I am trying to learn from the book"
+var b = " A smarter Way To Learn JavaScript"
+var c = a+b
+alert(c)
+var content = "Yah! I can Write HTML Content Through JavaScript"
+document.write(content)
+var imc = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(imc)
